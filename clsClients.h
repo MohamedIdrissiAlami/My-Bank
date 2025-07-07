d0571f276c9ffd10db787267f156fa7ed5661734
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-07 19:27:38 +0100

    create clsPerson Class

---
+++ clsClients.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "My-Cpp-Libraries/clsDate/clsDate.h";
+#include "My-Cpp-Libraries/clsInputValidate/clsInputValidate.h";
+#include "My-Cpp-Libraries/clsString/clsString.h";
+#include "My-Cpp-Libraries/clsUtil/clsUtil.h";
+#include "clsPerson.h";
+class clsClients :private clsPerson
+{
+};
+
---
+++ clsPerson.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <iostream>
+class clsPerson
+{
+private:
+	std::string _FirstName;
+	std::string _LastName;
+	std::string _Email;
+	std::string _Phone;
+
+public:
+	//get and set properties :
+	void SetFirstName(std::string FirstName)
+	{
+		this->_FirstName = FirstName;
+	}
+	std::string GetFirstName()
+	{
+		return this->_FirstName;
+	}
+
+	void  SetLastName(std::string LastName)
+	{
+		this->_LastName = LastName;
+	}
+	std::string GetLastName()
+	{
+		return this->_LastName;
+	}
+
+	void SetEmail(std::string  Email)
+	{
+		this->_Email = Email;
+	}
+	std::string GetEmail()
+	{
+		return this->_Email;
+	}
+
+	void SetPhone(std::string Phone)
+	{
+		this->_Phone = Phone;
+	}
+	std::string GetPhone()
+	{
+		return this->_Phone;
+	}
+
+	//get and set properties through '=' : 
+	__declspec(property(get = GetFirstName, put = SetFirstName)) std::string FirstName;
+	__declspec(property(get = GetLastName, put = SetLastName)) std::string  LastName;
+	__declspec(property(get = GetEmail, put = SetEmail)) std::string Email;
+	__declspec(property(get = GetPhone, put = SetPhone)) std::string Phone;
+
+
+	std::string  FullName()
+	{
+		return this->FirstName + " " + this->LastName;
+	}
+};
+
