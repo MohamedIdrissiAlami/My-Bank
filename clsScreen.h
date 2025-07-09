c18d9f534acfb9eb15ede1ffe9d8105d0a9ed59f
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-09 15:21:52 +0100

    clean up Drawscreenheader()

--- clsScreen.h
+++ clsScreen.h
@@ -5,21 +5,28 @@ using  namespace std;
 class clsScreen
 {
 public :
+
 	static void DrawScreenHeader(string Title, string SubTitle = "")
 	{
 		system("cls");
-		cout << "\n\n";
-		cout << setw(100) << "____________________________________________________________";//60
-		cout << "\n\n";
+		cout << "\n";
+		DrawHorizontalBorder(40, '_', 60);
+		cout << "\n";
 		cout << setw(40) << "|" << setw(40) << Title;
 		cout << "\n\n";
-
 		cout << setw(40) << "|" << setw(35) << SubTitle;
-		cout << "\n\n";
-		cout << setw(100) << "____________________________________________________________";//60
-		cout << "\n\n";
+		cout << "\n";
+		DrawHorizontalBorder(40, '_', 60);
+		cout << "\n";
+	}
 
 
+	static void DrawHorizontalBorder(short Margin, char chUnit , short BorderLength)
+	{
+		string Line = "";
+		for (short i = 1; i <= BorderLength; i++)
+			Line += chUnit;
+		cout << "\n" << setw(Margin) <<"" << Line << endl;
 	}
 };
 
