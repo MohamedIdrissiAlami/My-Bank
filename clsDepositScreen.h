9497df8c160d34e037c9fc1f589f17bcc8e95521
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-10 12:48:53 +0100

    implement clsDepositScreen

--- clsDepositScreen.h
+++ clsDepositScreen.h
@@ -1,11 +1,59 @@
 #pragma once
 #include "clsScreen.h"
+#include  "./My-Cpp-Libraries/clsInputValidate/clsInputValidate.h"
 class clsDepositScreen :private clsScreen
 {
+	static string ReadAccountNumber()
+	{
+		cout << "\n" << setw(40) << "";
+		return clsInputValidate::ReadString("Enter The client's Account Number ..");
+	}
+	static float _ReadDepositAmount()
+	{
+		return clsInputValidate::ReadPositiveNumber<float>("\nEnter deposit amount : ");
+	}
+
+	static void _PrintClientCard(clsClient Client)
+	{
+		cout << "\n" << setw(40) << "" << "The following is the Client's card : ";
+		cout << "\n" << setw(100) << "____________________________________________________________";//60
+		cout << "\n" << setw(40) << "|" << "\tFull Name      : " << Client.FullName();
+		cout << "\n" << setw(40) << "|" << "\tAccount number : " << Client.AccountNumber;
+		cout << "\n" << setw(40) << "|" << "\tAccount Balance: " << Client.AccountBalance;
+		cout << "\n" << setw(100) << "____________________________________________________________";//60
+
+	}
+
 public :
 	static void  ShowDepositScreen()
 	{
-		cout << "\ndeposit screen will be here..";
+		//cout << "\nDeposit screen will be here..";
+		clsScreen::DrawScreenHeader("D E P O S I T", "S C R E E N");
+		clsClient Client = clsClient::Find(ReadAccountNumber());
+		char Answer = 'n';
+		if (!Client.IsEmptyClientObject())
+		{
+			_PrintClientCard(Client);
+			float DepositAmount  = _ReadDepositAmount();
+			cout << "\nAre you sure do you want to perform this transaction ? [Y/N] ";
+			cin >> Answer;
+			if (tolower(Answer) == 'y')
+			{
+				if (Client.Deposit(DepositAmount))
+				{
+					cout << "\nDone successfully!";
+					cout << "\nnew balance : " << Client.AccountBalance;
+				}
+				else
+					cout << "\ncouldn't perform this transaction!";
+
+			}
+			else
+				cout << "\nCanceled!";
+		}
+		else
+			cout << "\n" << setw(40) << "" << "the client with [" << Client.AccountNumber << "] was Not found!:-(";
+
 	}
 };
 
