a066e7ac2374999cda37e3959f69283b38492470
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-11 15:29:09 +0100

    create find user screen stup

---
+++ clsFindUserScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "clsScreen.h"
+class clsFindUserScreen:private clsScreen
+{
+public :
+	static void ShowFindUserScreen()
+	{
+		cout << "\nfind user screen will be here..";
+	}
+};
+
