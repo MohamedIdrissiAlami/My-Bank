da6d885a90fd468bf04fc42f15229a1c766daa80
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-11 15:29:35 +0100

    create update  user screen stup

---
+++ clsUpdateUserScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "clsScreen.h"
+class clsUpdateUserScreen:private clsScreen
+{
+public :
+	static void ShowUpdateUserScreen()
+	{
+		cout << "\nupdate user screen will be here..";
+	}
+};
+
