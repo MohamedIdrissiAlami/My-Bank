057187ee5f38e6f5531d130a651fba407a457d82
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-11 15:28:16 +0100

    create add new user screen stup

---
+++ clsAddNewUserScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "clsScreen.h"
+class clsAddNewUserScreen :private clsScreen
+{
+public :
+	static void ShowAddNewUserScreen()
+	{
+		cout << "\nadd new  user screen will be here..";
+	}
+};
+
