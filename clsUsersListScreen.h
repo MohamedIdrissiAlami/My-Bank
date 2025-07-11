56adff406c417fbb445c37dec955f899f09c0517
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-11 15:30:05 +0100

    create users list screen stup

---
+++ clsUsersListScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "clsScreen.h"
+class clsUsersListScreen:private clsScreen
+{
+public :
+	static void ShowUsersListScreen()
+	{
+		cout << "\nusers list  screen will be here..";
+	}
+};
+
