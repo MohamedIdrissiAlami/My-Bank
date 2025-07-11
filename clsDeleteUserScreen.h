091645aee258febe34b0f7f0d13148ccc7f173d4
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-07-11 15:28:40 +0100

    create delete user screen stup

---
+++ clsDeleteUserScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "clsScreen.h"
+class clsDeleteUserScreen :private clsScreen
+{
+public  :
+	static void ShowDeleteUserScreen()
+	{
+		cout << "\ndelete user screen will be here..";
+	}
+};
+
